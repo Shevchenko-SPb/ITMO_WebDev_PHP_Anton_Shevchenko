858f92670e86769deab4e86189aa072c6839e6cc
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-16 01:08:08 +0300

    Second homework

--- index.php
+++ index.php
@@ -1,52 +1,58 @@
 <?php
-//var_dump($_GET);
-//
-//$action =  empty($_GET['action'])? 'default': $_GET['action'];
-//
-//var_dump($action);
-//
-//if (empty($_GET["action"])) {
-//    $action = "default";
-//} else {
-//    $action = $_GET["action"];
-//}
-//var_dump($action);
 
-//var_dump($_GET["status"]);
-//
-//switch ($_GET["status"]) {
-//    case 1:
-//        echo "new game";
-//        break;
-//    case "1":
-//        echo "new game2";
-//        break;
-//    case 2:
-//        echo "save game";
-//        break;
-//    case 3:
-//        echo "close game";
-//        break;
-//}
+// First task
 
-//$obj = new StdClass();
-//$obj->name = 'Hello';
-//
-//$output = "";
-//$action = "default";
-//function render($output, $str='!')
-//{
-//    $output = $output . $str;
-//}
-//
-//render($obj);
-//print($obj->name);
-
-$call = "render";
-function render () {
-    print("Чтото");
+$year = 2016;
+if ((($year % 4 != 0) && ($year % 400 != 0)) || $year % 100 == 0) {
+print ("$year год не является високосным");
+} else {
+    print ("$year год является високосным");
 }
-render();
-$call();
-"render"();
+
+// Second task
+
+switch ($_GET['action']) {
+    case 1:
+        print ("Выполняется команда 1");
+        break;
+    case 2:
+        print ("Выполняется команда 2");
+        break;
+    case 3:
+        print ("Выполняется команда 3");
+        break;
+    default:
+        print ("Неверная команда");
+}
+
+// Third task
+
+function getDaysToNewYear () {
+    $currentYear = date("Y");
+    $daysAfterNewYear = date("z");
+
+    if (($currentYear % 4 != 0) && (($currentYear % 400 != 0)) || ($currentYear % 100 == 0)) {
+        return 365 - $daysAfterNewYear;
+    } else {
+        return 366 - $daysAfterNewYear;
+    }
+}
+print (getDaysToNewYear ());
+
+// Fourth task
+
+print ("Факториал:");
+function factorial ($x) {
+    print ("$x <br>");
+    return $x === 0 ? 1 : $x * factorial($x - 1);
+}
+print factorial(7);
+
+// Fifth task
+
+function getPathOfAlias ($convertPath) {
+    return realpath($convertPath);
+}
+print getPathOfAlias("
+PhpstormProjects.ITMO_WebDev_PHP_Anton_Shevchenko.index.php");
 
