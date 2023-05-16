957876fed764e3446f60dd76963659a68398e0c0
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-16 15:24:44 +0300

    Second homework

--- index.php
+++ index.php
@@ -56,3 +56,29 @@ function getPathOfAlias ($convertPath) {
 print getPathOfAlias("
 PhpstormProjects.ITMO_WebDev_PHP_Anton_Shevchenko.index.php");
 
+## Переменное число параметров функции (устаревший способ)
+function myecho()
+{
+    foreach (func_get_args() as $v) {
+        print ("$v </br>");
+    }
+}
+
+// Отображаем строки одну под другой
+myecho("Меркурий", "Венера", "Земля", "Марс");
+
+function silly()
+{
+    $i = mt_rand(); // записывает в $i случайное число
+    echo "$i<br />"; // выводит его на экран
+// Эта $i не имеет к глобальной $i никакого отношения!
+}
+for ($i = 0; $i != 10; $i++) silly();
+
+$a = 5;
+function test()
+{
+    global $a;
+    return $a = 10;
+}
+print test();
