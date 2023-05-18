ea1203717a2214836aad3251034d4ea349a41c37
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-18 20:57:41 +0300

    1

--- login.php
+++ login.php
@@ -1,27 +1,29 @@
 <?php
 
-session_start();
 
-$login = 'admin';
-$passwd = '123';
 
-$user_login = $_POST['login'];
-$user_passwd = $_POST['passwd'];
-
-if ($user_login !== $login || $user_passwd !== $passwd) {
-
-    echo 'Login or password is incorrect! Please input try again!';
-    usleep(4000);
-    header('Location: ./index.php');
-} else if (!($user_login && $user_passwd)) {
-    echo 'Login or password is empty! Please input both.';
-    usleep(4000);
-    header('Location: ./index.php');
-} else {
-    $_SESSION['is_auth'] = true;
-    $_SESSION['str'] = 'sdafasdfsdf';
-    echo 'All is right!';
-    header('Location: ./admin.php');
-}
+//session_start();
+//
+//$login = 'admin';
+//$passwd = '123';
+//
+//$user_login = $_POST['login'];
+//$user_passwd = $_POST['passwd'];
+//
+//if ($user_login !== $login || $user_passwd !== $passwd) {
+//
+//    echo 'Login or password is incorrect! Please input try again!';
+//    usleep(4000);
+//    header('Location: ./index.php');
+//} else if (!($user_login && $user_passwd)) {
+//    echo 'Login or password is empty! Please input both.';
+//    usleep(4000);
+//    header('Location: ./index.php');
+//} else {
+//    $_SESSION['is_auth'] = true;
+//    $_SESSION['str'] = 'sdafasdfsdf';
+//    echo 'All is right!';
+//    header('Location: ./admin.php');
+//}
 
 
