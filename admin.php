e74fbf4c7803521fb4d48ecaf0678c1a957f97ab
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-18 18:36:59 +0300

    end lesson

--- admin.php
+++ admin.php
@@ -1,1 +1,8 @@
 <?php
+
+session_start();
+if (isset($_SESSION['is_auth']) && $_SESSION['is_auth'] == true ){
+    echo 'Welcome!';
+} else {
+    header('Location: ./login.php');
+}
--- index.php
+++ index.php
@@ -1,66 +1,10 @@
 <?php
-
-// var_dump($_GET);
-
-$action =  empty($_GET['action'])? 'default':  $_GET['action'];
-
-// var_dump($action);
-
-// var_dump($_GET['status']);
-
-// switch ($_GET['status']) {
-//    case 1:
-//       echo "Новая игра";
-//       break;
-//    case '1':
-//       echo "Новая игра";
-//       break;
-//    case 2:
-//       echo "Сохранить игру";
-//       break;
-//    case 3:
-//       echo "Выход из  игры";
-//       break;
-//    }
-// $obj = new StdClass();
-// $obj->name =  'Hello';
-// $output = "";
-// function render(&$output, $str='!') {
-//    $output = $output . $str;
-// }
-// function about($output) {
-//    print("Страница о нас!");
-// }
-// // render($output,'Hello');
-// // render($output, ' World');
-// $action($output);
-// print($output);
-
-
-
-// if (empty($_GET['action'])) {
-//    $action =  'default';
-
-// } else {
-//    $action = $_GET['action'];
-// }
-
-
-
-
-$x = 10;
-$y = 20;
-if (isset($y)){
-    print("ddd");
-}   else {
-    print("Help");
-}
-
-
-// function render($x)  {
-//       if is_null($x) return NULL
-//      print("Что то ! ");
-
-// }
-// $call = render();
-// var_dump($call == NULL);
+session_start();
+echo "<form action='login.php' method='POST'>
+<input name='login'></input>
+<input name='passwd'></input>
+<button>Login</button>
+</form>";
+// # https://phpabstract.ru/php/19
+// #https://www.php.net/manual/ru/reserved.variables.cookies.php
+// #https://www.php.net/manual/ru/features.cookies.php$_COOKIE["name"] -->
