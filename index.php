208b4bc78628d2f75a273c8891d5be4ad264e99a
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-21 20:36:08 +0300

    1

--- index.php
+++ index.php
@@ -2,34 +2,43 @@
 
 // Task 1
 
-$kilo = 1;
-$funt = 1;
-$i = 1;
+$directory = "./testDirectory/";
 
-for ($kilo = 1; $kilo <= 10; $kilo++) {
-    $funt = $kilo * 0.453;
-    echo "$kilo киллограмм равно - ";
-    echo "$funt фунтов <br>";
+function catalog ($dir)
+{
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) == true) {
+                if (is_dir($file)) {
+                    var_dump(filetype($file));
+                    echo "<br>";
+                }
+            }
+        }
+    }
 }
-for ($funt = 1; $funt <= 10; $funt++) {
-    $kilo = $funt / 0.453;
-    echo "$funt фунтов равно - ";
-    echo "$kilo киллограммов <br>";
-}
+catalog($directory);
+
+
+
+//    if ($path = opendir($path)) {
+//        while (($file = readdir($path)) !== false) {
+//            echo "файл: $file, тип: " . filetype($path . $file) . "\n";
+//            var_dump($path);
+//            if (is_dir($file)) {
+//                opendir($path . $file);
+//                var_dump($file);
+//            }
+//        }
+//        closedir($path);
+//    }
+
+
+
+
 
 // Task 2
 
-$fairy = 20;
-$plates = 10;
-echo "у нас тарелок $plates штук и фейри $fairy литров <br>";
-while ($plates > 0 && $fairy > 0) {
-    $plates -= 1;
-        $fairy -= 0.5;
-        echo "Тарелок осталось $plates , а ";
-        echo "фейри осталось $fairy <br>";
-
-}
-echo "итого осталось $plates тарелок и $fairy литров фейри =)";
 
 // Task 3
 
