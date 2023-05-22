f02bee7a3882e260e9a8f867fe43bc3fe95499d1
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-22 23:41:22 +0300

    1

--- index.php
+++ index.php
@@ -4,27 +4,39 @@
 
 $directory = "./testDirectory/";
 
-function catalog ($dir)
-{
-    if (is_dir($dir)) {
-        if ($dh = opendir($dir)) {
-            while (($file = readdir($dh)) == true) {
-                if (is_dir($file)) {
-                    var_dump(filetype($file));
-                    echo "<br>";
-                }
-
-            }
-        }
-    }
-}
-catalog($directory);
+//function catalog ($dir)
+//{
+//    if (is_dir($dir)) {
+//        if ($dh = opendir($dir)) {
+//            while (($file = readdir($dh)) == true) {
+//                if (is_dir($file)) {
+//                    var_dump(filetype($file));
+//                    echo "<br>";
+//                }
+//
+//            }
+//        }
+//    }
+//}
+//catalog($directory);
 
 // Task 2
 
-include "template/index.html";
+//include "template/index.html";
 
 // Task 3
 
+function getPathOfAlias($url) {
+    if (isset($url)){
+        var_dump("true");
+    return (realpath($url));}
+    else {
+        var_dump("false");
+        return false;
+    }
+}
+getPathOfAlias($directory);
 
+function import($directory) {
 
+}
