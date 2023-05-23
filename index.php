ab144d034d811ccbbf15980ede23640f48090d1f
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-23 19:03:00 +0300

    1

--- index.php
+++ index.php
@@ -4,21 +4,20 @@
 
 $directory = "./testDirectory/";
 
-//function catalog ($dir)
-//{
-//    if (is_dir($dir)) {
-//        if ($dh = opendir($dir)) {
-//            while (($file = readdir($dh)) == true) {
-//                if (is_dir($file)) {
-//                    var_dump(filetype($file));
-//                    echo "<br>";
-//                }
-//
-//            }
-//        }
-//    }
-//}
-//catalog($directory);
+function catalog ($dir)
+{
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) == true) {
+                if (is_dir($file)) {
+                    var_dump(filetype($file));
+                    echo "<br>";
+                }
+            }
+        }
+    }
+}
+catalog($directory);
 
 // Task 2
 
@@ -26,17 +25,17 @@ $directory = "./testDirectory/";
 
 // Task 3
 
-function getPathOfAlias($url) {
-    if (isset($url)){
-        var_dump("true");
-    return (realpath($url));}
-    else {
-        var_dump("false");
-        return false;
-    }
-}
-getPathOfAlias($directory);
-
-function import($directory) {
-
-}
+//function getPathOfAlias($url) {
+//    if (isset($url)){
+//        var_dump("true");
+//    return (realpath($url));}
+//    else {
+//        var_dump("false");
+//        return false;
+//    }
+//}
+//getPathOfAlias($directory);
+//
+//function import($directory) {
+//
+//}
