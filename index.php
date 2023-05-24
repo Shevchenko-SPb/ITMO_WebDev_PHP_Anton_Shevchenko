c05cbd5b5a50f8ad413c8880b1b0785636305af0
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-24 18:36:47 +0300

    end of the lesson

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+
+
+var_dump($_GET['controller']);
+var_dump($_GET['action']);
+//exit(0);
+$controller = $_GET['controller'];
+$controller_class = './controller' . ucfirst($controller) . '.php';
+$action = $_GET['action'];
+
+require_once $controller_class;
+
+
+$obj = new $controller('a', 'b');
+$obj->$action();
+
+//$obj = new Page(True, "Это подвал");
+// $obj1 = new Page('Это заголовок 1' , "Это подвал 1");
+// //$obj2 = new Page();
+// echo $obj->header();
+//Page::footer();
--- view.php
+++ view.php
@@ -1,1 +1,17 @@
 <?php
+
+class View {
+    function render($path, $data ) {
+        extract($data);
+        $fullpath = "./template/" . $path;
+        if (file_exists($fullpath) ) {
+            ob_start();
+            include $fullpath;
+            #$page = !$output?ob_get_clean():true;
+            $page = ob_get_clean();
+        } else {
+            throw new Exception("File does't exist!", 1);
+        }
+        echo $page;
+    }
+}
